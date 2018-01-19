0b1d6bedf1533152680fcb7193edf72acf4d5376
st036920 <st036920@712-I18-23.psd.provo.edu>
2018-01-19 12:54:13 -0700

    heyo it go

--- src/package_01/ImageReader.java
+++ src/package_01/ImageReader.java
@@ -1,10 +1,26 @@
 package package_01;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 public class ImageReader {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		BufferedImage image = ImageIO.read(new File("H:\\rsz_1one.png"));
+		int[][] colors = new int [28][28];
+	
+		for(int y = 0; y < 28; y++)
+		{
+			for(int x = 0; x < 28; x++)
+			{
+				int pixcolor = image.getRGB(x, y);
+				pixcolor = pixcolor & 0xFF;
+				System.out.println(pixcolor);
+				colors[x][y] = pixcolor;
+			}
+		}
 	}
-
 }
+
+
