5cc3af2aca937d657a2e6da1fbf8534fbbcc9b16
sadietayler <34242750+sadietayler@users.noreply.github.com>
2018-01-24 01:05:37 -0700

    Add files via upload

---
+++ src/package_01/CoolerImageReader.java
@@ -0,0 +1,48 @@
+package package_01;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class CoolerImageReader {
+
+    public static void main(String args[]) throws Exception {
+    	
+    	String dataString = "";
+		try {
+			dataString = new String(Files.readAllBytes(Paths.get("C:\\Users\\annie\\Desktop\\Sadie's Programming Stuff\\mnist_train_100.csv")));
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		int[][] pixels = new int [28][28];
+		String[] dataArray = dataString.split(",");
+		
+		int label;
+		int i = 0;
+		//outer loop doesn't work :( 
+		for(int image = 0; image < 100; image++)
+		{
+			label = Integer.parseInt(dataArray[i]);
+			System.out.println(label);
+			i++;
+			for(int y = 0; y < 28; y++)
+			{
+				for(int x = 0; x < 28; x++)
+				{
+					pixels[y][x] = Integer.parseInt(dataArray[i]);
+					System.out.print(pixels[y][x] + " ");
+					i++;
+				}
+				System.out.println();
+			}
+		}
+		
+    }
+}
+/* Things to do: 
+ * --make a bufferedimage out of the array so we can actually see what it looks like
+ * --figure out why the outer for loop is broken -_-
+ */
+
