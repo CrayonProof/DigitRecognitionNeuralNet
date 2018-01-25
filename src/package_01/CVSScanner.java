cd0637f195857657802aa6a36267a0b643a2818e
CrayonProof <mickyg1980@gmail.com>
2018-01-24 23:24:18 -0700

    Replaces magic numbers with constants

--- src/package_01/CVSScanner.java
+++ src/package_01/CVSScanner.java
@@ -8,18 +8,22 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class CVSScanner {
+	
+	private static int IMAGE_COUNT = 100;
+	private static int IMAGE_HIGHT = 28;
+	private static int IMAGE_WIDTH = 28;
 
 
 	public static void main(String[] args) throws FileNotFoundException 
 	{
-		int[][][] testImages = new int[100][28][28]; //image index, pixel y, pixel x
-		int[] labels = new int[100];
+		int[][][] testImages = new int[IMAGE_COUNT][IMAGE_HIGHT][IMAGE_WIDTH]; //image index, pixel y, pixel x
+		int[] labels = new int[IMAGE_COUNT];
 		Scanner in = new Scanner(new File(System.getProperty("user.dir") + "/mnist_train_100.csv"));
 		
 		int i;
 		
 		//for every image, creates a string and parses through the string, adding every digit to a 2d array
-		for (int image = 0; image < 100; image ++)
+		for (int image = 0; image < IMAGE_COUNT; image ++)
 		{
 			String line = in.nextLine();
 			String[] lineArray = line.split(",");
@@ -29,9 +33,9 @@ public class CVSScanner {
 			
 			i = 1;
 			
-			for (int y = 0; y < 28; y++)
+			for (int y = 0; y < IMAGE_HIGHT; y++)
 			{
-				for (int x = 0; x < 28; x++)
+				for (int x = 0; x < IMAGE_WIDTH; x++)
 				{
 					testImages[image][y][x] = Integer.parseInt(lineArray[i]);
 					i++;
@@ -45,18 +49,18 @@ public class CVSScanner {
 		//prints out the images from the array
 		while (true)
 		{
-			int imageNum = 101;
+			int imageNum = IMAGE_COUNT + 1;
 			Scanner req = new Scanner(System.in);
 			
-			while (imageNum > 100)
+			while (imageNum > IMAGE_COUNT)
 			{
-			System.out.println("Which image (1 - 100) would you like to display?");
+			System.out.println("Which image (1 - " + IMAGE_COUNT + ") would you like to display?");
 			imageNum = req.nextInt();
 			}
 			
-			for (int y = 0; y < 28; y++)
+			for (int y = 0; y < IMAGE_HIGHT; y++)
 			{
-				for (int x = 0; x < 28; x++)
+				for (int x = 0; x < IMAGE_WIDTH; x++)
 				{
 					if (testImages[imageNum - 1][y][x] == 0)
 					{
